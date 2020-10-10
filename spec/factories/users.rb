c02346b123d57f109885419f308eaa35e9266072
SWillames <sergiowoc@gmail.com>
2020-10-10 10:42:42 -0300

    User factory created.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
+require 'ffaker'
+
 FactoryBot.define do
   factory :user do
-    
+    name { FFaker::Name.name }
+    email{ FFaker::Internet.email }
+    password { FFaker::Internet.password }
   end
 end
