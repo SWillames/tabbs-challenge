7b71004d82d94657dbccb9b39329c572e70335da
SWillames <sergiowoc@gmail.com>
2020-10-10 10:44:07 -0300

    Unitary test(User).

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Validations" do
+    let(:user) { create(:user) } 
+    it { is_expected.to validate_presence_of(:name) } 
+    
+    it 'expected validate uniqueness of email' do
+      user1 = build(:user, email: user.email)
+      expect validate_uniqueness_of(:email)
+      expect(user1).not_to be_valid
+      expect(user1.errors[:email]).to be_present
+    end
+  end
+  
+  context "association" do
+    it { is_expected.to (:company) }
+  end
 end
