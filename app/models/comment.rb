b3c7c2e1b2e3d08d9c7ee75fc574cbf439b26857
SWillames <sergiowoc@gmail.com>
2020-10-12 19:38:25 -0300

    Improve the visualization of comments and make their text mandatory.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :commentable, polymorphic: true
   belongs_to :parent, optional: true, class_name: "Comment"
+  validates_presence_of :body
 
   def comments
     Comment.where(commentable: commentable, parent_id: id)
